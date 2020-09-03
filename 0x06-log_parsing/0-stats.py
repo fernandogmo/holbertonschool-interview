a7df8993fa689c787f7e45c645eacd05b172f9b7
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-09-02 20:12:21 -0700

    Reverting last change

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -19,8 +19,7 @@ if __name__ == "__main__":
     try:
         for line in sys.stdin:
             status, size = map(int, line.split()[-2:])
-            if status in codes:
-                codes[status] += 1
+            codes[status] += 1
             total_size += size
             line_count += 1
             if line_count % 10 == 0:
