4652ad0bf0a31fb8cada994cb37f9da5ef4990c1
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-09-16 02:01:41 -0700

    Much faster (and shorter).

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -7,29 +7,13 @@
  */
 int is_palindrome(unsigned long n)
 {
-	divmod_t qr;
 	unsigned long rev = 0, x = n;
 
 	if (n < 10)
 		return (1);
+	do {
+		rev = (rev * 10) + x % 10;
+	} while (x /= 10);
 
-	while (x)
-	{
-		divmod(x, 10, &qr);
-		rev = (rev * 10) + qr.r;
-		x = qr.q;
-	}
 	return (n == rev);
-}
-
-/**
- * divmod - updates @qr with the quotient and remainder of @n and @d
- * @n: unsigned long dividend.
- * @d: unsigned int divisor.
- * @qr: pointer to divmod_t to update with values.
- */
-void divmod(unsigned long n, unsigned int d, divmod_t *qr)
-{
-	qr->q = n / d;
-	qr->r = n - (qr->q * 10);
 }
