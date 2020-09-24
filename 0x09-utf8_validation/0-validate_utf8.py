5c4d4a74c37cab010bd2f3d13a85c24738315c31
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-09-23 22:53:42 -0700

    First draft for Task 0

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+A method that determines if a given data set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    data = iter(data)
+    mask = int('0b11111111', 2)
+    for n in data:
+        n &= mask
+        if int('0x00', 16) <= n <= int('0x7f', 16):
+            continue
+        if n < int('0b11100000', 2):
+            n1 = next(data) & mask
+            if int('0b01111111', 2) < n1 < int('0b11000000', 2):
+                continue
+            else:
+                return False
+        elif n < int('0b11110000', 2):
+            n1 = next(data) & mask
+            n2 = next(data) & mask
+            if int('0b01111111', 2) < n1 < int('0b11000000', 2) and \
+                    int('0b01111111', 2) < n2 < int('0b11000000', 2):
+                continue
+            else:
+                return False
+        elif n < int('0b11111000', 2):
+            n1 = next(data) & mask
+            n2 = next(data) & mask
+            n3 = next(data) & mask
+            if int('0b01111111', 2) < n1 < int('0b11000000', 2) and \
+                    int('0b01111111', 2) < n2 < int('0b11000000', 2) and \
+                    int('0b01111111', 2) < n3 < int('0b11000000', 2):
+                continue
+            else:
+                return False
+    return True
+
+
+if __name__ == "__main__":
+    pass
