51e6bc62fcf8b893ddac1817815a87fd91c652e5
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-10-08 22:01:00 -0700

    Trimmed down version that passes checker requirements

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -2,12 +2,11 @@
 
 /**
  * menger - draws a 2D Menger Sponge, AKA a Sierpinksi Carpet
- * @level: how many levels of Menger sponge to be drawn
+ * @level: dimension of Menger sponge to be drawn
  */
 void menger(int level)
 {
 	int i, j, n, size = pow(3, level);
-	char x = -1;
 
 	for (i = 0; i < size; ++i)
 	{
@@ -17,9 +16,7 @@ void menger(int level)
 				if (((i % (n * 3)) / n == 1) &&
 				    ((j % (n * 3)) / n == 1))
 					break;
-			if (!n) for (++x; x > 25; x %= 26);
-			putchar(n ? ' ' : (x + 65));
-			putchar(n ? ' ' : (++x + 65));
+			putchar(n ? ' ' : '#');
 		}
 		putchar('\n');
 	}
