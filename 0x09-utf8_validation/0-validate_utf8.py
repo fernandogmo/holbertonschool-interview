e7f7051e394ce190a3c785bdc12608ad0a18dc42
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2021-02-18 17:25:33 -0800

    Zeroing excess bits in each integer. Making all python source files executable to pass checker requirement.

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -7,10 +7,13 @@ A method that determines if a given data set represents a valid UTF-8 encoding
 def validUTF8(data):
     """
     `data` is a list of integers where each integer represents a byte
+
     Implementation:
         Here I'm trying the built-in standard library
         methods to see if that's enough for the checker.
     """
+    # I'm only using the lower 8 bits of each integer.
+    data = [n & 255 for n in data]
     try:
         bytes(data).decode()
     except (ValueError, UnicodeError):
