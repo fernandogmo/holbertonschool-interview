b98f5f51aa91aba3bd1b156c47ae8527a786b6f7
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-02-25 19:45:20 -0800

    Cleaning loop for slide_right

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -11,9 +11,10 @@ int slide_line(int *line, size_t size, int direction)
 {
 	if (line && size)
 		return (direction ? slide_right(line, size)
-						  : slide_left(line, size));
+				  : slide_left(line, size));
 	return (0);
 }
+
 
 /**
  * slide_left - slides values in array @line to left
@@ -51,6 +52,7 @@ int slide_left(int *line, size_t size)
 	return (1);
 }
 
+
 /**
  * slide_right - slides values in array @line to right
  * @line: int array of powers of 2
@@ -60,15 +62,14 @@ int slide_left(int *line, size_t size)
 int slide_right(int *line, size_t size)
 {
 	int i = size - 1, j = size - 2;
-	int last = line[i];
+	int last = line[i], curr = line[j];
 
-	for (; j >= 0; --j)
-	{
-		if (line[j])
+	for (; j >= 0; curr = line[--j])
+		if (curr != 0)
 		{
 			if (!last)
-				last = line[j];
-			else if (last == line[j])
+				last = curr;
+			else if (last == curr)
 			{
 				line[i--] = last * 2;
 				last = 0;
@@ -76,10 +77,9 @@ int slide_right(int *line, size_t size)
 			else
 			{
 				line[i--] = last;
-				last = line[j];
+				last = curr;
 			}
 		}
-	}
 
 	if (last)
 		line[i--] = last;
