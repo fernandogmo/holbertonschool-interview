6815ee55efe3414dc4fc8ccbedb6854e2a4032a4
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-04-01 20:16:43 -0700

    Initial commit

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,40 @@
+#include "search.h"
+
+/**
+ * linear_skip - skip linked list
+ * @list: linked list with express lane
+ * @value: value to be searched
+ * Return: node with @value or NULL if @value does not exist in @list
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node, *jump;
+
+	if (list == NULL)
+		return (NULL);
+
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
