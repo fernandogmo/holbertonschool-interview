f64967306d77b225ce5904a77f76283dd17f0aa7
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-04-22 18:15:52 -0700

    Removed global :(

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,7 +1,5 @@
 #include "sort.h"
 
-static size_t sz;
-
 /**
  * sift_down - swaps a node (in a heap array) that is 'too small' with
  * *		its largest child (thereby moving it down) until it is
@@ -9,15 +7,19 @@ static size_t sz;
  * @a: int pointer to first element of heap array
  * @root: size_t index of root node
  * @end: size_t limit of heap (in int array @a)
+ * @sz: size_t size of total size to pass to print_array function
  */
-static inline void sift_down(int *a, size_t root, size_t end)
+static inline void sift_down(int *a, size_t root, size_t end, size_t sz)
 {
 	size_t child;
 	while ((child = 2 * root + 1) <= end)
 	{
-		if (child < end && a[child] < a[child + 1]) child++;
-		if (a[root] >= a[child]) break;
-		SWAP(a[root], a[child]), print_array(a, sz);
+		if (child < end && a[child] < a[child + 1])
+			child++;
+		if (a[root] >= a[child])
+			break;
+		SWAP(a[root], a[child]);
+		print_array(a, sz);
 		root = child;
 	}
 }
@@ -29,16 +31,17 @@ static inline void sift_down(int *a, size_t root, size_t end)
  */
 void heap_sort(int *a, size_t n)
 {
-	sz = n; /* set global */
+	size_t sz = n;
 	if (a && n > 1)
 	{
 		size_t i = (n - 2) / 2;
 		while (i--) /* heapify */
-			sift_down(a, i, n);
+			sift_down(a, i, n, sz);
 		while (--n)
 		{
-			SWAP(a[0], a[n]), print_array(a, sz);
-			sift_down(a, 0, n - 1);
+			SWAP(a[0], a[n]);
+			print_array(a, sz);
+			sift_down(a, 0, n - 1, sz);
 		}
 	}
 }
