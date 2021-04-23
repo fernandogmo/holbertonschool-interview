3b718ee21d50bc38cccf15e104df6dd0bea27b5e
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-04-22 18:49:05 -0700

    Handling edge case: sorted array

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -14,10 +14,8 @@ static inline void sift_down(int *a, size_t root, size_t end, size_t sz)
 	size_t child;
 	while ((child = 2 * root + 1) <= end)
 	{
-		if (child < end && a[child] < a[child + 1])
-			child++;
-		if (a[root] >= a[child])
-			break;
+		if (child < end && a[child] < a[child + 1]) child++;
+		if (a[root] >= a[child]) break;
 		SWAP(a[root], a[child]);
 		print_array(a, sz);
 		root = child;
@@ -31,12 +29,12 @@ static inline void sift_down(int *a, size_t root, size_t end, size_t sz)
  */
 void heap_sort(int *a, size_t n)
 {
-	size_t sz = n;
-	if (a && n > 1)
+	size_t sz = n; /* save array size for printing */
+	if (a && n >= 2)
 	{
-		size_t i = (n - 2) / 2;
+		size_t i = 1 + (n - 2) / 2;
 		while (i--) /* heapify */
-			sift_down(a, i, n, sz);
+			sift_down(a, i, n - 1, sz);
 		while (--n)
 		{
 			SWAP(a[0], a[n]);
