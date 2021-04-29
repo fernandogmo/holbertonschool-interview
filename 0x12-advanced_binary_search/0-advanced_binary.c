a4c7af3e5413af2e962f1f8022784241ee17b935
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-04-29 07:33:06 -0700

    Updated function description

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -2,7 +2,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in an array of ints
+ * advanced_binary - searches for a value in an array of ints
  * @array: ptr to the first element of the array to search
  * @sz: number of elements in @array
  * @v: value to find in @array
