1e50199107eba65c1c466a867a63982d87fbc481
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-08-19 22:09:57 -0700

    0x21: Initial commit

---
+++ 0x21-substring/substring.h
@@ -0,0 +1,10 @@
+#ifndef SUBSTRING_H
+#define SUBSTRING_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif /* SUBSTRING_H */
